5146db3480c57434db8e35ea27a01d93f021320e
medrifai <rifai.moh.fst@uhp.ac.ma>
2024-04-27 13:41:25 +0100

    LoginController V1 by RIFAI

--- src/application/App.java
+++ src/application/App.java
@@ -1,33 +1,34 @@
 package application;
 
-import application.controllers.BackofficeSceneController;
+// import application.controllers.BackofficeSceneController;
+import application.controllers.LoginSceneController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+// import javafx.stage.StageStyle;
 
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         // Load the FXML file
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../resources/interfaces/BackofficeScene.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../resources/interfaces/LoginScene.fxml"));
         Parent root = loader.load();
 
         // Get the controller
-        BackofficeSceneController controller = loader.getController();
+        LoginSceneController controller = loader.getController();
         // Initialize the controller
         controller.initialize();
 
         // Set the scene
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
+        // primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setTitle("Crestwood College");
         primaryStage.setResizable(false);
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    public static void main(String[] args) {launch(args);}
 }
--- src/application/controllers/LoginSceneController.java
+++ src/application/controllers/LoginSceneController.java
@@ -1,10 +1,73 @@
 package application.controllers;
 
+import java.io.IOException;
+import java.sql.ResultSet;
+import application.database.SqlConnection;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 public class LoginSceneController {
+    @FXML
+    private TextField emailInput;
+    @FXML 
+    private PasswordField passwordInput;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Label errorMssg;
 
     public void initialize() {
-        
+        // Ajouter un écouteur d'événements pour la touche "Entrée" sur le champ de mot de passe
+        passwordInput.setOnKeyPressed(event -> {
+            if (event.getCode().toString().equals("ENTER")) {
+                // Appeler une méthode de validation de connexion
+                loginButtonAction(new ActionEvent());
+            }
+        });
+    }
+    public void loginButtonAction(ActionEvent e){
+        if(emailInput.getText().isBlank() == false && passwordInput.getText().isBlank() == false){
+            // errorMssg.setText("Processus de connexion en cours");
+            validateLogin();
+        } else{
+            errorMssg.setText("Please Enter your Email and Password !");
+        }
+    }
+    public void validateLogin() {
+        try {
+            java.sql.Connection connectDB = SqlConnection.getConnection();
+            String verifyLogin = "SELECT count(1) FROM adsministrateur WHERE email = '" + emailInput.getText() + "' AND motDePass = '" + passwordInput.getText() + "' ";
+            java.sql.Statement statement = connectDB.createStatement();
+            ResultSet queryResult = statement.executeQuery(verifyLogin);
 
+            if (queryResult.next()) {
+                int count = queryResult.getInt(1);
+                if (count == 1) {
+                    // Connexion réussie
+                    openHomePgae();
+                } else {
+                    // Échec de connexion
+                    errorMssg.setText("Email or Password are wrong. Please try again !");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    public void openHomePgae(){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("../../resources/interfaces/BackofficeScene.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) loginButton.getScene().getWindow(); 
+            stage.setScene(scene);
+            stage.show();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
